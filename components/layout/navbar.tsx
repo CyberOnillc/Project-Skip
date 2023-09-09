f32b38a34881896e8d9838f94995c379eabaad07
TechWithTy <tyrique1.daniel@gmail.com>
2023-09-09 11:58:20 -0600

    fixed mobile menu layout and functionality

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC,useState,useEffect,useRef } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
@@ -8,6 +8,7 @@ import { Session } from "next-auth";
 import { ShoppingCart, User, Search } from "lucide-react";
 import DropdownMenu from "../shared/dropdown";
 import Image from "next/image";
+import Sidebar from "../shared/sidebar";
 interface NavBarProps {
   session: Session | null;
 }
@@ -46,34 +47,38 @@ const NavBar: FC<NavBarProps> = ({ session }) => {
   const scrolled = useScroll(50);
   const navBarRef = useRef<HTMLElement>(null);
 
-const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(null);
+  const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(
+    null,
+  );
 
-const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-useEffect(() => {
-  function handleClickOutside(event: MouseEvent) {
-    const target = event.target as Element;
-    if (!target.closest(".dropdown")) {
-      if (!isButtonClicked) {
-        setOpenDropdownIndex(null);
-      } else {
-        setIsButtonClicked(false);
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const target = event.target as Element;
+      if (!target.closest(".dropdown")) {
+        if (!isButtonClicked) {
+          setOpenDropdownIndex(null);
+        } else {
+          setIsButtonClicked(false);
+        }
       }
     }
-  }
 
-  document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
-  return () => {
-    document.removeEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isButtonClicked]);
+
+  const toggleDropdown = (index: number | null = null) => {
+    setIsButtonClicked(true);
+    setOpenDropdownIndex(openDropdownIndex === index ? null : index);
   };
-}, [isButtonClicked]);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
-  
-const toggleDropdown = (index: number | null = null) => {
-  setIsButtonClicked(true);
-  setOpenDropdownIndex(openDropdownIndex === index ? null : index);
-};
   return (
     <>
       <SignInModal />
@@ -84,8 +89,9 @@ const toggleDropdown = (index: number | null = null) => {
             : "bg-white/0"
         } z-30 p-4 transition-all`}
       >
-        <div className="mx-auto flex h-16 w-full max-w-screen-xl items-center justify-between">
+        <div className="mx-auto flex h-16 w-full max-w-screen-xl flex-wrap items-center justify-between">
           <div className="flex items-center">
+            {" "}
             <Link href="/">
               <Image
                 src="/Website_Simple_Logo_1.png"
@@ -97,7 +103,9 @@ const toggleDropdown = (index: number | null = null) => {
             </Link>
             <span>Project Skip</span>
           </div>
-          <div className="flex justify-center space-x-4">
+
+          {/* Desktop Menu */}
+          <div className="hidden justify-center space-x-4 md:flex">
             {menuItemsArray.map((menuItems, index) => (
               <DropdownMenu
                 key={index}
@@ -107,7 +115,9 @@ const toggleDropdown = (index: number | null = null) => {
               />
             ))}
           </div>
-          <div className="flex items-center space-x-4">
+
+          {/* ... Icons and Login/Signup buttons code ... */}
+          <div className="hidden items-center space-x-4 md:flex">
             <ShoppingCart />
             <User />
             <Search />
@@ -116,13 +126,13 @@ const toggleDropdown = (index: number | null = null) => {
             ) : (
               <>
                 <button
-                  className=" rounded px-4 py-2 text-black  underline"
+                  className="rounded px-4 py-2 text-black underline"
                   onClick={() => setShowSignInModal(true)}
                 >
                   Login
                 </button>
                 <button
-                  className="bg-gradient-purple rounded px-4 py-2 text-white"
+                  className="rounded bg-gradient-purple px-4 py-2 text-white"
                   onClick={() => setShowSignInModal(true)}
                 >
                   Sign Up
@@ -130,6 +140,53 @@ const toggleDropdown = (index: number | null = null) => {
               </>
             )}
           </div>
+
+          {/* Hamburger menu icon for mobile */}
+          <div className="flex items-center md:hidden">
+            <button onClick={() => setIsSideBarOpen(true)}>
+              {/* Simple Hamburger Icon */}≡
+            </button>
+          </div>
+
+          {/* Mobile Menu */}
+          {isSideBarOpen && (
+            <Sidebar isOpen={isSideBarOpen} setIsSideBarOpen={setIsSideBarOpen}>
+              <div className=" left-0 top-full mt-2 flex w-full flex-col space-y-4 rounded bg-white shadow-lg md:hidden">
+                {menuItemsArray.map((menuItems) =>
+                  menuItems.map((item) => (
+                    <Link href={item.MenuLinkURL} key={item.MenuLinkName}>
+                      <span className="block px-4 py-2 hover:bg-gray-200">
+                        {item.MenuLinkName}
+                      </span>
+                    </Link>
+                  )),
+                )}
+                <div className="flex flex-col items-center space-y-4">
+                  <ShoppingCart />
+                  <User />
+                  <Search />
+                  {session ? (
+                    <UserDropdown session={session} />
+                  ) : (
+                    <>
+                      <button
+                        className="rounded px-4 py-2 text-black underline"
+                        onClick={() => setShowSignInModal(true)}
+                      >
+                        Login
+                      </button>
+                      <button
+                        className="rounded bg-gradient-purple px-4 py-2 text-white"
+                        onClick={() => setShowSignInModal(true)}
+                      >
+                        Sign Up
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
+            </Sidebar>
+          )}
         </div>
       </nav>
     </>
---
+++ components/shared/sidebar.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode, useState } from "react";
+
+interface SidebarProps {
+    children: ReactNode;
+    isOpen: Boolean;
+    setIsSideBarOpen: (value: boolean) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children, isOpen,setIsSideBarOpen }) => {
+
+  return (
+    <div>
+      {/* Button to open the sidebar */}
+      <button onClick={() => setIsSideBarOpen(true)}>Open Sidebar</button>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed right-0 top-0 h-full w-full max-w-md transform bg-white shadow-lg transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="relative h-full">
+          <button
+            className="absolute right-4 top-4 z-10"
+            onClick={() => setIsSideBarOpen(false)}
+          >
+            ×
+          </button>
+          {/* Sidebar content */}
+          <div className="p-4">{children}</div>
+        </div>
+      </div>
+
+      {/* Overlay when sidebar is open */}
+    </div>
+  );
+};
+
+export default Sidebar;
