851db0e021160c88d2ed5d76ab3491f36b527e47
nehal7-ml <nehal.sk.99@gmail.com>
2023-11-13 21:27:53 +0530

    added padding to card price

--- components/CourseCard/index.tsx
+++ components/CourseCard/index.tsx
@@ -32,7 +32,7 @@ const CourseCard = ({ course }: CourseCardProps) => {
                 </div>
                 <h2 className="text-base font-semibold line-clamp-1  lg:line-clamp-2">{course.name}</h2>
 
-                <div className="flex justify-between">
+                <div className="flex justify-between py-3">
                     <p className="text-gray-600">{course.price}</p>
                     <button className="mt-4  text-gray-800 py-2 px-4 rounded-full">
                         <ShoppingCart></ShoppingCart>
